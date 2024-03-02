7334b1f23834259eabc11c547436ebd69c82954d
koseadem <adem@evam.com>
2024-03-02 14:53:17 +0300

    first commit

--- target/classes/schema.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS `presentation` (
-    `id` bigint(20) NOT NULL auto_increment,
-    `name` varchar(200) NOT NULL,
-    `minute` bigint(20) NOT NULL,
-    PRIMARY KEY (`id`)    
-);
